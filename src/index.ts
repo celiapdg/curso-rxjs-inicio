d2145a136f5907772688259ace44a8041652823b
Celia <celia2795@gmail.com>
2023-05-23 10:25:07 +0200

    subscribe y observer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs";
+import { Observable, Observer } from "rxjs";
 
 
 
@@ -20,10 +20,56 @@ const obs$ = new Observable<string>(subs => {
 obs$.subscribe(console.log);
 
 
+const obs2$ = new Observable<string>(subs => {
+
+    subs.next('Patata');
+    subs.next('Pepino');
+
+    // Forzar un error
+    const a = undefined;
+    a.nombre = 'Fernando';
+
+    subs.complete();
+
+    // una vez completada, ya no se emite
+    subs.next('Patata');
+    subs.next('Pepino');
+
+});
 
 
+obs2$.subscribe({
+    next: valor => console.log('next:', valor),
+    error: error => console.warn('error:', error),
+    complete: () => console.info('completed')
+});
 
 
+// OBSERVER
+
+const observer: Observer<string> = {
+    next: value => console.log('siguiente [next]:', value),
+    error: error => console.warn('error [obs]:', error),
+    complete: () => console.info('Completado [obs]')
+}
+
+const obs3$ = new Observable<string>(subs => {
+
+    subs.next('Patata');
+    subs.next('Pepino');
+
+    // Forzar un error
+    const a = undefined;
+    a.nombre = 'Fernando';
+
+    subs.complete();
+
+    // una vez completada, ya no se emite
+    subs.next('Patata');
+    subs.next('Pepino');
+
+});
 
 
+obs3$.subscribe(observer);
 
