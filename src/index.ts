42e2505ecbc4a099103b585dfc457f1924ad106a
Celia <celia2795@gmail.com>
2023-05-23 10:10:45 +0200

    inicio observables

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
+import { Observable } from "rxjs";
 
 
-console.log('Hola Mundo!');
+
+// const obs$ = Observable.create(); 
+const obs$ = new Observable<string>(subs => {
+
+    subs.next('Patata');
+    subs.next('Pepino');
+
+    subs.complete();
+
+    // una vez completada, ya no se emite
+    subs.next('Patata');
+    subs.next('Pepino');
+
+});
+
+// obs$.subscribe(resp => console.log(resp));
+obs$.subscribe(console.log);
+
+
 
 
 
